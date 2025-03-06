1437ae138b3b30e1baaf62f657fec3a2dd97a243
hasnat_kallol <hasnatkallol1971@gmail.com>
2025-03-06 15:33:58 +0600

    Done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('product-container').addEventListener('click',function(e){
+     if (e.target.tagName === 'BUTTON'){
+        let button = e.target.parentNode.parentNode.children[1].children[1].innerText;
+        // console.log(button);
+     }
+       
+        
+})
